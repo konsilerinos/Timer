2c6b4ddf1eeeac9f579b6fe68acdcf1aa71813d7
konsilerinos <konsilerinos@gmail.com>
2021-11-08 17:12:47 +0300

    Timer updated

---
+++ Sources/time25successwindow.cpp
@@ -0,0 +1,31 @@
+#include "time25successwindow.h"
+#include "mainwindow.h"
+#include "ui_time25successwindow.h"
+#include <QWidget>
+
+Time25SuccessWindow::Time25SuccessWindow(QWidget* parent) : QDialog(parent), ui(new Ui::Time25SuccessWindow) {
+
+    this->setWindowFlags(Qt::CustomizeWindowHint | Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+
+    // Инициализация медиапроигрывателя
+    player = new QMediaPlayer;
+    player->setMedia(QUrl("qrc:/Resources/Sounds/Revolt.wav"));
+    player->setVolume(50);
+    player->play();
+
+    ui->setupUi(this);
+}
+
+Time25SuccessWindow::~Time25SuccessWindow() {
+
+    delete player;
+    delete ui;
+    delete this;
+}
+
+// Выход, нажатие
+void Time25SuccessWindow::on_vamosButton_clicked() {
+
+    player->stop();
+    QWidget::close();
+}
